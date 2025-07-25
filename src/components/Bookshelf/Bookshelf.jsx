a292082ec39076ae0925ad3a54f6aa9e86744c90
SylviaRemington <sylviaremington@gmail.com>
2025-07-25 16:12:53 -0700

    setting up functions of handleInputChange and handleSubmit to create event handlers

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
 
 const Bookshelf = () => {
-
     const [books, setbooks] = useState([
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
         { title: 'Automate the Boring Stuff With Python', author: 'Al Sweigart'},
     ]);
-
     const [newBook, setNewBook] = useState({ title: '', author: ''});
 
+    // function handleInputChange
+    const handleInputChange = (input) => {
+
+    }
+
+    // function handleSubmit
+    const handleSubmit = (input) => {
+
+    }
 
 
     return <div className="bookshelfDiv">
 
         <div className="formDiv">
             <h3>Add a Book</h3>
+            {/* FORM CREATION */}
             {/* Form will go here */}
         </div>
 
