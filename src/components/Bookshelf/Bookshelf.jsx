690696ebf59cb57cbe8ce33cca5760fa286d577f
SylviaRemington <sylviaremington@gmail.com>
2025-07-25 16:01:05 -0700

    defined the initial state section - lab exercise 2

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,6 +1,17 @@
 import { useState } from 'react';
 
 const Bookshelf = () => {
+
+    const [books, setbooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+        { title: 'Automate the Boring Stuff With Python', author: 'Al Sweigart'},
+    ]);
+
+    const [newBook, setNewBook] = useState({ title: '', author: ''});
+
+
+
     return <div className="bookshelfDiv">
 
         <div className="formDiv">
