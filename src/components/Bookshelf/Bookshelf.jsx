c46135cf9bb74d5278a13a2c57714670001bf2c5
SylviaRemington <sylviaremington@gmail.com>
2025-07-25 16:36:16 -0700

    creating handleInputChange function - lab exercise 3a

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -8,15 +8,35 @@ const Bookshelf = () => {
     ]);
     const [newBook, setNewBook] = useState({ title: '', author: ''});
 
-    // function handleInputChange
-    const handleInputChange = (input) => {
 
-    }
+    // function handleInputChange
+    // Using (event) because every time the user will type in the form, this will be registered as an event for my function.
+    const handleInputChange = (event) => {
+        const name = event.target.name; //name in the form input box (e.g. the 'title' or 'author' part)
+        const value = event.target.value; //what the user typed in
+
+        const updatedBook = {
+            title: newBook.title,
+            author: newBook.author,
+        };
+
+        updatedBook[name] = value; //this updates the one field the user typed in
+
+        setNewBook(updatedBook); //new state - this setter is saving newBook to a new state as an updatedBook.
+    };
+    
+    // A More Advanced Version of handleInputChange below:
+    //Looked up a more advanced way to do it & found the following. Don't understand all of this yet. But note to self to research and understand further:
+    // const handleInputChange = (event) => {
+    //     const {name, value} = event.target;
+    //     setNewBook({ ...newBook, [name]:value});
+    // };
+
 
     // function handleSubmit
-    const handleSubmit = (input) => {
+    // const handleSubmit = (event) => {
 
-    }
+    // }
 
 
     return <div className="bookshelfDiv">
