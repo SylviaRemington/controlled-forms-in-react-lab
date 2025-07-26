59987a8df6edad174de6689d9f4f06ee0cb7daa2
SylviaRemington <sylviaremington@gmail.com>
2025-07-25 21:11:06 -0700

    put h1 and Bookshelf tag in a div in App.jsx & then able to style all together in conjunction without weird spacing

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
 
 const App = () => {
   return (
-    <>
+    <div className="mainContainer">
       <h1>My Bookshelf</h1>
       <Bookshelf />
-    </>
+    </div>
   )
 };
 
