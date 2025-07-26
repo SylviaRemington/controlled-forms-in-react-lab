3a5cca4e8b112c54f601c45076d2233901bf4cfd
SylviaRemington <sylviaremington@gmail.com>
2025-07-25 19:21:11 -0700

    checking handleInputChange is working by running console log and adding fake onClick until I get the controlled form set up - working

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -13,7 +13,11 @@ const Bookshelf = () => {
     // Using (event) because every time the user will type in the form, this will be registered as an event for my function.
     const handleInputChange = (event) => {
         const name = event.target.name; //name in the form input box (e.g. the 'title' or 'author' part) -- The event is accessing the name of the input field.
+        console.log("Checking const name is working:", name);
         const value = event.target.value; //what the user typed in - This is accessing the value the user has typed.
+        console.log("Checking value is working:", value);
+
+        console.log("newBook BEFORE change:", newBook);
 
         // const updatedBook = {
         //     title: newBook.title,
@@ -26,10 +30,14 @@ const Bookshelf = () => {
             [name]: value //updating one field at a time - “Look at what’s inside the variable called name, and use that as the field (key) name. Then store value as the value for that field.”
         };
 
+        console.log("updatedBook AFTER spread + update:", updatedBook);
+
         // ! I think this below line 30 is doing the same thing as name & value above? Not sure if I need this too.
         updatedBook[name] = value; //this updates the one field the user typed in
+        console.log("updatedBook to see if it's the same as updatedBook AFTER", updatedBook);
 
         setNewBook(updatedBook); //new state - this setter is saving newBook to a new state as an updatedBook.
+        console.log("Checking new state to see if working:", updatedBook);
     };
 
     // A More Advanced Version of handleInputChange below:
@@ -55,6 +63,8 @@ const Bookshelf = () => {
         </div>
 
         <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        <button onClick={() => handleInputChange({ target: { name: "title", value: "value" } })}>
+            Test handleInputChange</button>
 
     </div>
 };
